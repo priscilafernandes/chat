fe62fa65eff60ed1efe9feb71d13b6e9e60c4807
Priscila Fernandes <pris1@outlook.com.br>
2019-02-03 21:19:56 -0200

    enter key funcionando

--- src/app.js
+++ src/app.js
@@ -7,38 +7,29 @@ class Msg {
 
   getMsg() {
     this.messageInput = messageInput.value;
-    const messageText = document.createTextNode(this.messageInput);
-    const messageItem = document.createElement('li');
-
+    let messageText = document.createTextNode(this.messageInput);
+    let messageItem = document.createElement('li');
     messageItem.appendChild(messageText);
     messageList.appendChild(messageItem);
 
     return this;
   }
-
-  submitKey(event) {
-    const key = event.keyCode || event.which;
-    const getMsgMethod = this.getMsg;
-
-    if (key === 13) {
-      console.log(getMsgMethod);
-      return getMsgMethod;
-    }
-    
-    return this;
-  }
   
-  submitMsg() {    
+  submitMsg() {
     this.submitButton.addEventListener('click', this.getMsg);
-    this.messageInput.addEventListener('keypress', this.submitKey);
+    this.messageInput.addEventListener('keypress', (e) => {
+      let key = e.keyCode || e.which;
+      if (key === 13) {
+        return this.getMsg.apply(this);
+      }
+    })
 
     return this;
   }
 }
 
-const messageInput = document.getElementById('message-input');
-const messageList = document.getElementById('message-list');
-const submitButton = document.getElementById('submit-button');
-
-const message = new Msg(messageInput, messageList, submitButton);
+let messageInput = document.getElementById('message-input');
+let messageList = document.getElementById('message-list');
+let submitButton = document.getElementById('submit-button');
+let message = new Msg(messageInput, messageList, submitButton);
 message.submitMsg();
